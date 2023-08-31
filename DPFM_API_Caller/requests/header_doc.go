6d84d2143b2e446e6f25a232c74191515b93fed4
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2023-08-31 16:54:55 +0900

    no message

---
+++ DPFM_API_Caller/requests/header_doc.go
@@ -0,0 +1,16 @@
+package requests
+
+type HeaderDoc struct {
+	InspectionLot		 	 int	`json:"InspectionLot"`
+	Operations		 	 	 int	`json:"Operations"`
+	OperationsItem		 	 int	`json:"OperationsItem"`
+	OperationID		 	 	 int	`json:"OperationID"`
+	ConfirmationCountingID 	 int	`json:"ConfirmationCountingID"`
+	DocType                  string `json:"DocType"`
+	DocVersionID             int    `json:"DocVersionID"`
+	DocID                    string `json:"DocID"`
+	FileExtension            string `json:"FileExtension"`
+	FileName                 string `json:"FileName"`
+	FilePath                 string `json:"FilePath"`
+	DocIssuerBusinessPartner int    `json:"DocIssuerBusinessPartner"`
+}
---
+++ DPFM_API_Caller/sql.go
@@ -0,0 +1,84 @@
+package dpfm_api_caller
+
+import (
+	dpfm_api_input_reader "data-platform-api-inspection-lot-confirmation-doc-reads-rmq-kube/DPFM_API_Input_Reader"
+	dpfm_api_output_formatter "data-platform-api-inspection-lot-confirmation-doc-reads-rmq-kube/DPFM_API_Output_Formatter"
+	"fmt"
+	"github.com/latonaio/golang-logging-library-for-data-platform/logger"
+)
+
+func (c *DPFMAPICaller) readSqlProcess(
+	input *dpfm_api_input_reader.SDC,
+	output *dpfm_api_output_formatter.SDC,
+	accepter []string,
+	errs *[]error,
+	log *logger.Logger,
+) interface{} {
+	var headerDoc *[]dpfm_api_output_formatter.HeaderDoc
+
+	for _, fn := range accepter {
+		switch fn {
+		case "HeaderDoc":
+			func() {
+				headerDoc = c.HeaderDoc(input, output, errs, log)
+			}()
+		}
+	}
+
+	data := &dpfm_api_output_formatter.Message{
+		HeaderDoc: 		headerDoc,
+	}
+
+	return data
+}
+
+func (c *DPFMAPICaller) HeaderDoc(
+	input *dpfm_api_input_reader.SDC,
+	output *dpfm_api_output_formatter.SDC,
+	errs *[]error,
+	log *logger.Logger,
+) *[]dpfm_api_output_formatter.HeaderDoc {
+	where := "WHERE 1 = 1"
+
+	if input.HeaderDoc.InspectionLot != nil {
+		where = fmt.Sprintf("%s\nAND InspectionLot = %d", where, *input.HeaderDoc.InspectionLot)
+	}
+	if input.HeaderDoc.Operations != nil {
+		where = fmt.Sprintf("%s\nAND Operations = %d", where, *input.HeaderDoc.Operations)
+	}
+	if input.HeaderDoc.OperationsItem != nil {
+		where = fmt.Sprintf("%s\nAND OperationsItem = %d", where, *input.HeaderDoc.OperationsItem)
+	}
+	if input.HeaderDoc.OperationID != nil {
+		where = fmt.Sprintf("%s\nAND OperationID = %d", where, *input.HeaderDoc.OperationID)
+	}
+	if input.HeaderDoc.ConfirmationCountingID != nil {
+		where = fmt.Sprintf("%s\nAND ConfirmationCountingID = %d", where, *input.HeaderDoc.ConfirmationCountingID)
+	}
+	if input.HeaderDoc.DocType != nil && len(*input.HeaderDoc.DocType) != 0 {
+		where = fmt.Sprintf("%s\nAND DocType = '%v'", where, *input.HeaderDoc.DocType)
+	}
+	if input.HeaderDoc.DocIssuerBusinessPartner != nil && *input.HeaderDoc.DocIssuerBusinessPartner != 0 {
+		where = fmt.Sprintf("%s\nAND DocIssuerBusinessPartner = %v", where, *input.HeaderDoc.DocIssuerBusinessPartner)
+	}
+	groupBy := "\nGROUP BY InspectionLot, Operations, OperationsItem, OperationID, ConfirmationCountingID, DocType, DocIssuerBusinessPartner "
+
+	rows, err := c.db.Query(
+		`SELECT
+    	InspectionLot, Operations, OperationsItem, OperationID, ConfirmationCountingID, DocType, MAX(DocVersionID), DocID, FileExtension, FileName, FilePath, DocIssuerBusinessPartner
+		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_inspection_lot_confirmation_header_doc_data
+		` + where + groupBy + `;`)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+	defer rows.Close()
+
+	data, err := dpfm_api_output_formatter.ConvertToHeaderDoc(rows)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+
+	return data
+}
